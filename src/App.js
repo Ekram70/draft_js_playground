5ecab8c2aa9fb1cf73dca63cd28b2a5bdcaa3740
Ekram70 <ekramullah70@gmail.com>
2022-11-02 22:18:29 +0600

    working with editor component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { Editor } from 'react-draft-wysiwyg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Editor
+        wrapperClassName="m_editor_wrapper"
+        editorClassName="m_editor"
+        toolbarClassName="m_editor_toolbar"
+      />
+    </>
   );
 }
 
